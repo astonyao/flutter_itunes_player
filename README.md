f217a2f28e348a08cf5b537d27a9e94da6776d10
Aston Yao <ayao@dius.com.au>
2020-10-09 17:24:50 +1100

    add readme. remove unfinished tests.

--- README.md
+++ README.md
@@ -1,14 +1,48 @@
 # flutter_itunes_player
+A flutter app which allows users to search for music using by artist name. It shows information about the song as well as a thumbnail. The app can play a 30 seconds preview on selected songs.
+
+## Features implemented
+- Search and list songs by artist name.
+- Displays song’s title, artist, album and album art.
+- Play preview of 30 seconds
+- Indicator when a song is being played.
+- Indicator removal when a song finishes playing.
+- Pause and resume control.
+- Continues to play the current song when a new search is performed.
+- Play/pause control is only shown when a song is clicked.
+- returns up to 50 songs.
  
-## how to run this app?
+## Installation
+Clone this repo.
+install dependencies 
 
-## how to run the test
+```
+flutter pub get.
+```
+Run the app. (make sure you have Android simulator open at this point)
 
-## version of flutter quired, android phone required
+```
+flutter run
+```
 
-## what features i have done
+## How to run test
+```
+flutter test
+```
 
-## what i would like to do if i have more time
+## System requirement
+The app was developed using Flutter Channel stable version 1.22.0. It should support Android devices with SDK version from 23 to 29. I have only tested the app using Android simulator but it should work on iOS too.
 
-## how long i spent
+## Time spent
+6 hours
 
+## Improvements
+Due to time constraint, I didn't have as many tests as I like to have. Here is a list of things I would improve/test if I had more time.
+
+- Implement state management solution (BLoC or Provider) to separate business logic from UI.
+- Spend more time comparing different audio player plugins, and find the most suitable one.
+- Test Song model conversion from raw JSON input.
+- Test music player's different UI states when different song data is provided.
+	- eg. Mock `songs` data with two songs in it, and make sure expected information are there.
+	- Mock the API call with various status code returned (200, 202, 400, 404....), and make sure the app handles errors correctly.
+	- Break search bar and songs list into two different files, similar to what I did to `MusicController`.
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -15,57 +8,7 @@ import 'package:flutter_itunes_player/main.dart';
 class MockDio extends Mock implements Dio {}
 
 void main() {
-  Dio client = MockDio();
-
-  Response<dynamic> failedAPIResponse = Response<dynamic>(
-    statusCode: 404,
-    data: null,
-  );
-
-  Response<dynamic> mockAPIResponse = Response<dynamic>(
-    statusCode: 200, 
-    data: <String, dynamic> {
-    'resultCount':1,
-    'results':[
-        {
-          'wrapperType':'track',
-          'kind':'song',
-          'artistId':271256,
-          'collectionId':1440642493,
-          'trackId':1440643139,
-          'artistName':'Drake',
-          'collectionName':'Take Care (Deluxe Version)',
-          'trackName':'The Motto (feat. Lil Wayne) [Bonus Track]',
-          'collectionCensoredName':'Take Care (Deluxe Version)',
-          'trackCensoredName':'The Motto (feat. Lil Wayne) [Bonus Track]',
-          'artistViewUrl':'https://music.apple.com/us/artist/drake/271256?uo=4',
-          'collectionViewUrl':'https://music.apple.com/us/album/the-motto-feat-lil-wayne-bonus-track/1440642493?i=1440643139&uo=4',
-          'trackViewUrl':'https://music.apple.com/us/album/the-motto-feat-lil-wayne-bonus-track/1440642493?i=1440643139&uo=4',
-          'previewUrl':'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview128/v4/11/8d/5c/118d5c80-9cf3-8763-164f-46c133ec6b2d/mzaf_4957978631652780821.plus.aac.p.m4a',
-          'artworkUrl30':'https://is2-ssl.mzstatic.com/image/thumb/Music128/v4/d4/df/9a/d4df9abd-ec9d-06c2-ac90-821630b8cb8b/source/30x30bb.jpg',
-          'artworkUrl60':'https://is2-ssl.mzstatic.com/image/thumb/Music128/v4/d4/df/9a/d4df9abd-ec9d-06c2-ac90-821630b8cb8b/source/60x60bb.jpg',
-          'artworkUrl100':'https://is2-ssl.mzstatic.com/image/thumb/Music128/v4/d4/df/9a/d4df9abd-ec9d-06c2-ac90-821630b8cb8b/source/100x100bb.jpg',
-          'collectionPrice':10.99,
-          'trackPrice':1.29,
-          'releaseDate':'2011-11-15T12:00:00Z',
-          'collectionExplicitness':'explicit',
-          'trackExplicitness':'explicit',
-          'discCount':1,
-          'discNumber':1,
-          'trackCount':20,
-          'trackNumber':19,
-          'trackTimeMillis':181560,
-          'country':'USA',
-          'currency':'USD',
-          'primaryGenreName':'Hip-Hop/Rap',
-          'contentAdvisoryRating':'Explicit',
-          'isStreamable':true
-        },
-    ]
-});
-
   testWidgets('Renders initial screen', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
     expect(find.byType(MaterialApp), findsOneWidget);
     expect(find.byType(MusicPlayer), findsOneWidget);
@@ -74,9 +17,4 @@ void main() {
     expect(find.byType(ListTile), findsNothing);
   });
 
-  group('Music player', () {
-    setUp(() {
-      
-    });
-  });
 }
